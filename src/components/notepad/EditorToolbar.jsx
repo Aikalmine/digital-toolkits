5d8b0a3e417a4a38d898dec9701d3403d49ecaa8
M. Ali <m.ali@pixako.com>
2022-12-28 16:28:40 +0500

    note pad trash completed

--- src/components/notepad/EditorToolbar.jsx
+++ src/components/notepad/EditorToolbar.jsx
@@ -10,7 +10,7 @@ import { getStorageValue, useLocalStorage } from '../common/useLocalStorage';
 // Custom Undo button icon component for Quill editor. You can import it directly
 // from 'quill/assets/icons/undo.svg' but I found that a number of loaders do not
 // handle them correctly
-const CustomUndo = () => (
+export const CustomUndo = () => (
   <svg viewBox="0 0 18 18">
     <polygon className="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10" />
     <path
@@ -21,7 +21,7 @@ const CustomUndo = () => (
 );
 
 // Redo button icon component for Quill editor
-const CustomRedo = () => (
+export const CustomRedo = () => (
   <svg viewBox="0 0 18 18">
     <polygon className="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10" />
     <path
@@ -32,7 +32,7 @@ const CustomRedo = () => (
 );
 
 // print button icon component for Quill editor
-const CustomPrint = () => (
+export const CustomPrint = () => (
 <svg width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 18 18"> 
   <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/> 
   <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
@@ -40,7 +40,7 @@ const CustomPrint = () => (
 );
 
 // cut button icon component for Quill editor
-const CustomCut = () => (
+export const CustomCut = () => (
   <FontAwesomeIcon icon={faScissors} />
 );
 
@@ -56,7 +56,7 @@ function stripHtml(text) {
 let copyText = getStorageValue('notepad');
 copyText = stripHtml(copyText);
 // copy button icon component for Quill editor
-const CustomCopy = () => (
+export const CustomCopy = () => (
   
   <div>
      <CopyToClipboard text={copyText}
@@ -68,7 +68,7 @@ const CustomCopy = () => (
 );
 
 // paste button icon component for Quill editor
-const CustomPaste = () => (
+export const CustomPaste = () => (
   <FontAwesomeIcon icon={faPaste} />
 );
 
@@ -98,16 +98,10 @@ function handleCutChange(){
 }
 
 
-function handleCopyChange(){
 
-}
 
 function handlePasteChange(){
 
-}
-
-function handleTrashChange(){
-  
 }
 
 // Add sizes to whitelist and register them
@@ -136,9 +130,7 @@ export const modules = {
       redo: handleRedoChange,
       print:handlePrintChange,
       cut:handleCutChange,
-      copy:handleCopyChange,
-      paste:handlePasteChange,
-      trash:handleTrashChange,
+      paste:handlePasteChange
     }
   },
   history: {
@@ -169,88 +161,3 @@ export const formats = [
   "color",
   "code-block"
 ];
-
-// Quill Toolbar component
-export const QuillToolbar = () => (
-  <div id="toolbar">
-    <span className="ql-formats">
-      <select className="ql-font" defaultValue="arial">
-        <option value="arial">Arial</option>
-        <option value="comic-sans">Comic Sans</option>
-        <option value="courier-new">Courier New</option>
-        <option value="georgia">Georgia</option>
-        <option value="helvetica">Helvetica</option>
-        <option value="lucida">Lucida</option>
-      </select>
-      <select className="ql-size" defaultValue="medium">
-        <option value="extra-small">Size 1</option>
-        <option value="small">Size 2</option>
-        <option value="medium">Size 3</option>
-        <option value="large">Size 4</option>
-      </select>
-      <select className="ql-header" defaultValue="3">
-        <option value="1">Heading</option>
-        <option value="2">Subheading</option>
-        <option value="3">Normal</option>
-      </select>
-    </span>
-    <span className="ql-formats">
-      <button className="ql-bold" />
-      <button className="ql-italic" />
-      <button className="ql-underline" />
-      <button className="ql-strike" />
-    </span>
-    <span className="ql-formats">
-      <button className="ql-list" value="ordered" />
-      <button className="ql-list" value="bullet" />
-      <button className="ql-indent" value="-1" />
-      <button className="ql-indent" value="+1" />
-    </span>
-    <span className="ql-formats">
-      <button className="ql-script" value="super" />
-      <button className="ql-script" value="sub" />
-      <button className="ql-blockquote" />
-      <button className="ql-direction" />
-    </span>
-    <span className="ql-formats">
-      <select className="ql-align" />
-      <select className="ql-color" />
-      <select className="ql-background" />
-    </span>
-    <span className="ql-formats">
-      <button className="ql-link" />
-      <button className="ql-image" />
-      <button className="ql-video" />
-    </span>
-    <span className="ql-formats">
-      <button className="ql-formula" />
-      <button className="ql-code-block" />
-      <button className="ql-clean" />
-    </span>
-    <span className="ql-formats">
-      <button className="ql-undo">
-        <CustomUndo />
-      </button>
-      <button className="ql-redo">
-        <CustomRedo />
-      </button>
-      <button className="ql-print">
-        <CustomPrint />
-      </button>
-      <button className="ql-cut">
-        <CustomCut />
-      </button>
-      <button className="ql-copy">
-        <CustomCopy />
-      </button>
-      <button className="ql-paste">
-        <CustomPaste />
-      </button>
-      <button className="ql-trash">
-        <ClearTextArea />
-      </button>
-    </span>
-  </div>
-);
-
-export default QuillToolbar;
--- src/components/notepad/Notepad.jsx
+++ src/components/notepad/Notepad.jsx
@@ -1,7 +1,9 @@
 import ReactQuill from 'react-quill';
-import EditorToolbar, { modules, formats } from "./EditorToolbar";
+import  { modules, formats, CustomUndo, CustomRedo, CustomPrint, CustomCut, CustomPaste, CustomCopy } from "./EditorToolbar";
 import 'react-quill/dist/quill.snow.css';
 import { useLocalStorage } from '../common/useLocalStorage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 //https://github.com/zenoamaro/react-quill
 function Notepad() {
@@ -12,9 +14,91 @@ function Notepad() {
         setNotepad(e);
     }
 
+    function handleClickClear(){
+        setNotepad('');
+    }
+
   return (
     <div className="notepad">
-        <EditorToolbar />
+        <div id="toolbar">
+            <span className="ql-formats">
+            <select className="ql-font" defaultValue="arial">
+                <option value="arial">Arial</option>
+                <option value="comic-sans">Comic Sans</option>
+                <option value="courier-new">Courier New</option>
+                <option value="georgia">Georgia</option>
+                <option value="helvetica">Helvetica</option>
+                <option value="lucida">Lucida</option>
+            </select>
+            <select className="ql-size" defaultValue="medium">
+                <option value="extra-small">Size 1</option>
+                <option value="small">Size 2</option>
+                <option value="medium">Size 3</option>
+                <option value="large">Size 4</option>
+            </select>
+            <select className="ql-header" defaultValue="3">
+                <option value="1">Heading</option>
+                <option value="2">Subheading</option>
+                <option value="3">Normal</option>
+            </select>
+            </span>
+            <span className="ql-formats">
+            <button className="ql-bold" />
+            <button className="ql-italic" />
+            <button className="ql-underline" />
+            <button className="ql-strike" />
+            </span>
+            <span className="ql-formats">
+            <button className="ql-list" value="ordered" />
+            <button className="ql-list" value="bullet" />
+            <button className="ql-indent" value="-1" />
+            <button className="ql-indent" value="+1" />
+            </span>
+            <span className="ql-formats">
+            <button className="ql-script" value="super" />
+            <button className="ql-script" value="sub" />
+            <button className="ql-blockquote" />
+            <button className="ql-direction" />
+            </span>
+            <span className="ql-formats">
+            <select className="ql-align" />
+            <select className="ql-color" />
+            <select className="ql-background" />
+            </span>
+            <span className="ql-formats">
+            <button className="ql-link" />
+            <button className="ql-image" />
+            <button className="ql-video" />
+            </span>
+            <span className="ql-formats">
+            <button className="ql-formula" />
+            <button className="ql-code-block" />
+            <button className="ql-clean" />
+            </span>
+            <span className="ql-formats">
+                <button className="ql-undo">
+                    <CustomUndo />
+                </button>
+                <button className="ql-redo">
+                    <CustomRedo />
+                </button>
+                <button className="ql-print">
+                    <CustomPrint />
+                </button>
+                <button className="ql-cut">
+                    <CustomCut />
+                </button>
+                <button className="ql-copy">
+                    <CustomCopy />
+                </button>
+                <button className="ql-paste">
+                    <CustomPaste />
+                </button>
+                <button onClick={handleClickClear} className="ql-trash">
+                    <FontAwesomeIcon icon={faTrash} />
+                </button>
+            </span>
+        </div>
         <div id="notepadText">
             <ReactQuill
                 theme="snow"
